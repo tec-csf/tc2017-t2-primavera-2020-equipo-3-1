007fc6fdaa7852da85381bfcdb1c2846ee1d91d0
EstebanManrique <a01027077@itesm.mx>
2020-03-14 14:02:30 -0600

    Menu de Usuario establecido como clase y uso de template para vector de elementos en arboles

--- Arboles.cpp
+++ Arboles.cpp
@@ -1,29 +1,50 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-void UserMenu()
+class AVLTree
 {
-    int choice = 0;
-    int numberOfElements = 1; //Se declara numero de elementos como 1 para no tener nunca un arbol vacio
 
-    do
-    {
-        cout<<"1) Arbol AVL     2) Arbol B     3) Terminar Programa"<<endl;
-        cin>>choice;
-        if(choice == 3)
+};
+
+class AVLNode
+{
+
+};
+
+class BTree
+{
+
+};
+template <class Number>
+class UserMenu
+{
+    public:
+        int choice = 0;
+        int numberOfElements = 1; //Se declara numero de elementos como 1 para no tener nunca un arbol vacio
+        vector<Number> numbers;
+
+        void UserInteraction()
         {
-            break;
+            do
+            {
+                cout<<"1) Arbol AVL     2) Arbol B     3) Terminar Programa"<<endl;
+                cin>>choice;
+                if(choice == 3)
+                {
+                    break;
+                }
+                cout<<"Introducir el numero de elementos deseados a ser manejados por el arbol: 10, 100, 1000, 10000, 100000, 1000000 o cualquier otro numero"<<endl;
+                cin>>numberOfElements;
+            }
+            while(choice!=3);  
         }
-        cout<<"Introducir el numero de elementos deseados a ser manejados por el arbol: 10, 100, 1000, 10000, 100000, 1000000 o cualquier otro numero"<<endl;
-        cin>>numberOfElements;
-    }
-    while(choice!=3);
-}
+};
 
 int main()
 {
-    UserMenu();
+    UserMenu <int> uS;
+    uS.UserInteraction();
     return 0;
-
 }
